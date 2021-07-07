54aaf7d099acc422cc225285946b341e94e16983
blyndusk <alexandre.delaloy.pro@gmail.com>
2021-07-07 12:00:14 +0200

    fix(es): #1 - conflicts

--- api/main.go
+++ api/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-	"github.com/blyndusk/elastic-books/api/router"
+	"github.com/blyndusk/elastic-books/api/es"
+	"github.com/blyndusk/elastic-books/api/helpers"
 )
 
 func main() {
-	// es.Search()
+	_, err := es.InitClient()
+	helpers.ExitOnError("client init", err)
+
 	router.Setup()
 }
