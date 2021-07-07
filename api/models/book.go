123f4c196f572792410f852a18b28eaf14c0749e
blyndusk <alexandre.delaloy.pro@gmail.com>
2021-07-07 15:05:44 +0200

    fix(model): #3 - book idd type

--- api/models/book.go
+++ api/models/book.go
@@ -1,7 +1,7 @@
 package models
 
 type Book struct {
-	Id     uint   `json:"id"`
+	Id     string   `json:"id"`
 	Name   string `json:"name"`
 	Author string `json:"author"`
 	Resume string `json:"resume"`
